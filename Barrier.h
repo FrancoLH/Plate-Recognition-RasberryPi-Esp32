48db3b448bb7ed1bb1a0381b91221dae0e59a9ea
Franco Heis <franco.heis2008@gmail.com>
2026-06-04 10:07:06 -0300

    Refactor main function and update I/O settings in Barrier.h

--- Barrier.h
+++ Barrier.h
@@ -1,43 +1,41 @@
+#pragma config FOSC  = XT      
+#pragma config WDTE  = OFF    
+#pragma config PWRTE = ON      
+#pragma config MCLRE = ON      
+#pragma config BOREN = ON      
+#pragma config LVP   = OFF     
+#pragma config CPD   = OFF     
+#pragma config CP    = OFF     
 
-#define _XTAL_FREQ 4000000
+#define _XTAL_FREQ 4000000     
 #include <xc.h>
-#define __delay_ms(x) 
 
-void main(void){
-    CMCON = 7;
-    TRISA = 0b00111111;
-    TRISB = 0b00110111;
-    TRISA7 = 0;
-    TRISB3 = 0;
-    TRISB4 = 0;
-    TRISB5 = 0;
-    T2CON = 0b00000101;
-    PR2 = 100;
-    CCPR1L = 0;
-    CCP1CON = 0b00001100;
-    int inicio = 0;
-    RB4 = 0;
-    RB7 = 0;
-    RB5 = 0;
-    int sentido = 0;
+char mi_letra = 'A';
 
-    while(1){
-        if (RB1==1){
-            inicio = !inicio;
-            __delay_ms(300);
-            sentido = 0;
-        }
-
-        if(RB2==1 && inicio == 1){
-            RB5 == 1;
-            __delay_ms(300);
-            sentido = 1;
-        }
-
-        if(RB3==1 && inicio == 1 && sentido == 1){
-            RB5==0;
-            __delay_ms(300);
-            sentido = 0;
+void main(void) {
+    CMCON = 0x07;
+    TRISB0 = 0;    
+    TRISB1 = 1;   
+    TRISB2 = 0;    
+    SPEN = 0;      
+    CREN = 0;      
+    TXEN = 0;     
+    RB0 = 0;
+    RB2 = 0;
+    while (1) {
+        if (RB1 == 0) {
+            if (mi_letra == 'A') {
+             RB2 = 1;   
+             RB0 = 0;   
+            } 
+            else {
+            RB0 = 1;   
+            RB2 = 0;   
+         }
+        while (RB1 == 0);
+         __delay_ms(20);   
+		RB0=0;
+		RB2=0;
+            }
         }
     }
-}
