c3cc06240d0e1574b007d6f5964122db8e7bd31a
Juani <tovojuanignacio@gmail.com>
2026-06-25 12:10:12 -0300

    Initialize new project structure for ESP32, adding necessary README files, platformio.ini, and main.cpp; remove old files. Create "Barrier" directory

---
+++ Barrera/src/main.cpp
@@ -0,0 +1,59 @@
+#include <Arduino.h>
+int motor1_pin1 = 27;
+int motor1_pin2 = 26;
+int enb = 25;
+String comando = "";
+int sensor_up = 0;
+int sensor_down = 0;
+bool lastSensorDown = HIGH;
+void setup() 
+{
+  pinMode(motor1_pin1, OUTPUT);
+  pinMode(motor1_pin2, OUTPUT);
+  pinMode(enb, OUTPUT);
+  pinMode(sensor_up, INPUT);
+  pinMode(sensor_down, INPUT);
+  Serial.begin(115200);
+}
+void subir()
+  {
+    digitalWrite(motor1_pin1, HIGH);
+    digitalWrite(motor1_pin2, LOW);
+    digitalWrite(enb, HIGH);
+  }
+  void bajar()
+  {
+    digitalWrite(motor1_pin1, LOW);
+    digitalWrite(motor1_pin2, HIGH);
+    digitalWrite(enb, HIGH);
+  }
+  void stop()
+  {
+    digitalWrite(motor1_pin1, LOW);
+    digitalWrite(motor1_pin2, LOW);
+    digitalWrite(enb, LOW);
+  }
+void loop() {
+  if(Serial.available())
+  {
+    comando = Serial.readStringUntil('\n');
+    comando.trim();
+  }
+
+  if(comando == "levantar barrera")
+  {
+      subir();
+  }
+  bool currentSensorDown = digitalRead(sensor_down);
+
+  if(lastSensorDown == HIGH && currentSensorDown == LOW)
+  {
+    bajar();
+  }
+
+  if(digitalRead(sensor_up) == HIGH)
+  {
+    stop();
+  }
+
+}
